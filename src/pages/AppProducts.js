a1f74049f0723973144455ad1475797738ffc330
milorad996 <mips96.1@gmail.com>
2022-06-24 19:17:39 +0200

    Zadatak 6

--- src/pages/AppProducts.js
+++ src/pages/AppProducts.js
@@ -6,14 +6,51 @@ import ProductsService from "../services/ProductsService";
 
 function AppProducts() {
 
+    const [product, setProduct] = useState('');
+
     const [products, setProducts] = useState(ProductsService.getAll())
+
+    const [query, setQuery] = useState('');
+    const search = (data) => {
+        return data.filter(
+            (item) =>
+                item.name.toLowerCase().includes(query)
+        )
+
+    }
+    const filter = (e) => {
+        const keyword = e.target.value;
+
+        if (keyword !== '') {
+            const results = products.filter((product) => {
+                return product.name.toLowerCase().startsWith(keyword.toLowerCase());
+            });
+            setProducts(results);
+        } else {
+            setProducts(products);
+        }
+
+        setProduct(keyword);
+    };
     return (
         <div>
-            <ul>
-                {products.map((product, index) => (
-                    <li key={index}>{product.name}</li>
-                ))}
-            </ul>
+
+            <input
+                type="search"
+                value={product}
+                onChange={filter}
+                className="input"
+                placeholder="Search"
+            />
+            {products && products.length > 0 ? (
+                products.map((product, index) => (
+                    <li key={index}>
+                        <span>{product.name}</span>
+                    </li>
+                ))
+            ) : (
+                <h1>No results found!</h1>
+            )}
         </div>
     )
 }
